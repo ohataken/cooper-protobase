ba52400305814b1109c9b403991775ec693acd31
Ken Ohata <ohataken@outlook.com>
2016-10-20 17:28:45 +0900

    update main

--- main.c
+++ main.c
@@ -7,10 +7,12 @@ void greeting() {
 
 int main(void) {
     printf("cooper-protobase\n");
-    struct cooper_object *a = new_cooper_object(7, NULL);
-    struct cooper_object *b = new_cooper_object(7, a);
+    struct cooper_object *a = new_cooper_object(1, NULL);
+    struct cooper_object *b = new_cooper_object(1, a);
 
     insert_cooper_object(a, "greeting", greeting);
+    insert_cooper_object(a, "hi", greeting);
+    insert_cooper_object(a, "hello", greeting);
 
     ((void (*)())lookup_cooper_object(b, "greeting"))();
 
